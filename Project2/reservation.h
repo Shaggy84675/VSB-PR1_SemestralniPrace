554b3691ba6b931cc92fff6deb4fa9d1875eb368
Patrik RainbowShaggy <admin@RainbowShaggy.com>
2017-12-05 20:44:33 +0100

    Přidána funkce na zrušení rezervace, vyhledání indexu dané rezervace a další drobné úpravy

--- Project2/reservation.h
+++ Project2/reservation.h
@@ -19,6 +19,7 @@ void PrintReservationsTable(vector <struct Room> &rooms_data, vector <struct Res
 bool FillReservationsStructure(string &path, vector <struct Reservation> &data);
 bool CheckReservationsIntegrity(string &path, vector <struct Reservation> &data);
 bool SaveReservationsStructure(string &path, vector <struct Reservation> &data);
-bool PrintFreeRooms(vector <struct Room> &rooms_data, vector <struct Reservation> &reservations_data);
-bool BookRoom(string &path, vector <struct Room> &rooms_data, vector <struct Reservation> &reservations_data);
+bool MakeReservation(string &path, vector <struct Room> &rooms_data, vector <struct Reservation> &reservations_data);
 bool IsRoomFree(int room_number, short day, short month, short year, vector <struct Room> &rooms_data, vector <struct Reservation> &reservations_data);
+int FindReservationIndex(int id, short day, short month, short year, vector <struct Reservation> &data);
+bool CancelReservation(string &path, vector <struct Room> &rooms_data, vector <struct Reservation> &reservations_data);
